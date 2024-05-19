fc5f72dc9471895343319a764a09f64cbd33cd0f
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-19 13:51:09 +0700

    Test the sign up page's title

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,5 +4,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_url
     assert_response :success
+    assert_select "title", full_title("Sign up")
   end
 end
